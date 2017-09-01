c6bf8d00b2ffb913a5da047bf8f9d4f9c2337468
David Webster <david.webster@eatnow.com.au>
2017-09-01 16:36:49 +1000

    Changes from running 2to3

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python
 
-from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
-import httplib
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import http.client
 import zlib
 import sys
 
 class RequestHandler(BaseHTTPRequestHandler):
     def do_GET(self):
-        conn = httplib.HTTPSConnection(REMOTE_SERVER)
+        conn = http.client.HTTPSConnection(REMOTE_SERVER)
         request_headers = dict()
         for header in self.headers:
-            print header, ':', self.headers[header]
+            print(header, ':', self.headers[header])
             value = self.headers[header]
             if header == "host":
                 value = REMOTE_SERVER
@@ -20,20 +20,20 @@ class RequestHandler(BaseHTTPRequestHandler):
             conn.request(self.command, self.path, headers=request_headers)
         else:
             request_body = self.rfile.read(int(self.headers.getheader('content-length')))
-            print request_body
+            print(request_body)
             conn.request(self.command, self.path, request_body, request_headers)
         res = conn.getresponse()
 
         self.send_response(res.status)
         for header in res.getheaders():
-            print header
+            print(header)
             self.send_header(header[0], header[1])
         self.end_headers()
         response_body = res.read()
         decompressed_body = response_body
         if res.getheader('content-encoding') == 'gzip':
             decompressed_body = zlib.decompress(response_body, 31)
-        print decompressed_body
+        print(decompressed_body)
         self.wfile.write(response_body)
         conn.close()
 
